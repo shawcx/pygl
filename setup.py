f482cb00ed3fe592a4bf946e3a3e088d51836446
Matthew Oertle <moertle@gmail.com>
2019-10-22 19:11:23 -0400

    fixup setup.py

--- setup.py
+++ setup.py
@@ -8,32 +8,32 @@ import setuptools
 defines = []
 include_dirs = ['/usr/local/include']
 library_dirs = ['/usr/local/lib']
-libs = ['GLEW']
+libs = ['GL', 'GLU', 'GLEW']
+
 
 setuptools.setup(
-    name             = 'GL',
-    version          = '0.1.0',
-    author           = 'Matthew Oertle',
-    author_email     = 'moertle@gmail.com',
-    url              = 'https://github.com/moertle/pygl',
-    license          = 'MIT',
-    description      = 'Python bindings for OpenGL',
+    name         = 'pygl',
+    version      = '0.1.0',
+    author       = 'Matthew Oertle',
+    author_email = 'moertle@gmail.com',
+    url          = 'https://github.com/moertle/pygl',
+    license      = 'MIT',
+    description  = 'Python bindings for OpenGL',
     long_description = open('README.rst').read(),
     ext_modules = [
         setuptools.Extension(
-            'GL',
+            'pygl',
             ['src/pygl.c'],
             define_macros = defines,
-            include_dirs = include_dirs,
-            library_dirs = library_dirs,
-            libraries = libs,
+            include_dirs  = include_dirs,
+            library_dirs  = library_dirs,
+            libraries     = libs
             )
         ],
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         ],
     )
