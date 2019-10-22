6458ab7d7d95874777ace34010c2fd77b68ace27
Matthew Oertle <moertle@gmail.com>
2019-10-22 19:02:32 -0400

    pygl python3 updates

--- test/gears.py
+++ test/gears.py
@@ -32,17 +32,176 @@
 
 
 #
-# Port of glxgears to test SDL/GL python bindings
-# By Matthew Oertle 2016
+# Port of glxgears to test SDL2 / GL python bindings
+# By Matthew Oertle 2019
 #
 
 import sys
 import math
 
-import SDL2 as SDL
-import GL
+import SDL2
+import pygl as GL
 
-window = None
+
+class Gears:
+    def __init__(self):
+        self.frames = 0
+        self.time0 = 0
+        self.gear1 = None
+        self.gear2 = None
+        self.gear3 = None
+        self.view_rotx = 20.0
+        self.view_roty = 30.0
+        self.view_rotz = 0.0
+        self.angle = 0.0
+
+        SDL2.Init(SDL2.INIT_VIDEO)
+
+        size = (300,300)
+        pos  = (SDL2.WINDOWPOS_CENTERED, SDL2.WINDOWPOS_CENTERED)
+        self.window = SDL2.Window("Gears", size, pos, SDL2.WINDOW_OPENGL | SDL2.WINDOW_RESIZABLE)
+        if not self.window:
+            print("Couldn't set 300x300 GL video mode: %s\n", SDL2.GetError())
+            SDL2.Quit()
+            sys.exit(2)
+
+        self.window.GL_CreateContext()
+
+        self.init()
+        self.reshape(300, 300)
+
+        done = False
+        while not done:
+            self.angle += 2.0
+
+            while True:
+                event = SDL2.PollEvent()
+                if not event:
+                    break
+
+                event,data = event
+
+                if event == SDL2.QUIT:
+                    done = True
+                    break
+
+                if event == SDL2.WINDOWEVENT:
+                    if data[0] == SDL2.WINDOWEVENT_SIZE_CHANGED:
+                        self.reshape(data[1],data[2])
+
+            keys = SDL2.GetKeyboardState()
+            if keys[SDL2.SCANCODE_ESCAPE]:
+                done = True
+            if keys[SDL2.SCANCODE_UP]:
+                self.view_rotx += 5.0
+            if keys[SDL2.SCANCODE_DOWN]:
+                self.view_rotx -= 5.0
+            if keys[SDL2.SCANCODE_LEFT]:
+                self.view_roty += 5.0
+            if keys[SDL2.SCANCODE_RIGHT]:
+                self.view_roty -= 5.0
+            if keys[SDL2.SCANCODE_Z]:
+                if SDL2.GetModState() & SDL2.KMOD_SHIFT:
+                    self.view_rotz -= 5.0
+                else:
+                    self.view_rotz += 5.0
+
+            self.draw()
+
+        del self.window
+        SDL2.Quit()
+
+    def init(self):
+        pos = [5.0, 5.0, 10.0, 0.0]
+        red = [0.8, 0.1, 0.0, 1.0]
+        green = [0.0, 0.8, 0.2, 1.0]
+        blue = [0.2, 0.2, 1.0, 1.0]
+
+        GL.Lightfv(GL.LIGHT0, GL.POSITION, pos)
+        GL.Enable(GL.CULL_FACE)
+        GL.Enable(GL.LIGHTING)
+        GL.Enable(GL.LIGHT0)
+        GL.Enable(GL.DEPTH_TEST)
+
+        # make the gears
+        self.gear1 = GL.GenLists(1)
+        GL.NewList(self.gear1, GL.COMPILE)
+        GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, red)
+        gear(1.0, 4.0, 1.0, 20, 0.7)
+        GL.EndList()
+
+        self.gear2 = GL.GenLists(1)
+        GL.NewList(self.gear2, GL.COMPILE)
+        GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, green)
+        gear(0.5, 2.0, 2.0, 10, 0.7)
+        GL.EndList()
+
+        self.gear3 = GL.GenLists(1)
+        GL.NewList(self.gear3, GL.COMPILE)
+        GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, blue)
+        gear(1.3, 2.0, 0.5, 10, 0.7)
+        GL.EndList()
+
+        GL.Enable(GL.NORMALIZE)
+
+        if len(sys.argv) > 1 and sys.argv[1] == "-info":
+            print("GL_RENDERER   =", GL.GetString(GL.RENDERER))
+            print("GL_VERSION    =", GL.GetString(GL.VERSION))
+            print("GL_VENDOR     =", GL.GetString(GL.VENDOR))
+            print("GL_EXTENSIONS =", GL.GetString(GL.EXTENSIONS))
+
+    # new window size or exposure
+    def reshape(self, width, height):
+        h = float(height) / float(width)
+
+        GL.Viewport(0, 0, width, height)
+        GL.MatrixMode(GL.PROJECTION)
+        GL.LoadIdentity()
+        GL.Frustum(-1.0, 1.0, -h, h, 5.0, 60.0)
+        GL.MatrixMode(GL.MODELVIEW)
+        GL.LoadIdentity()
+        GL.Translatef(0.0, 0.0, -40.0)
+
+    def draw(self):
+        GL.Clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
+
+        GL.PushMatrix()
+        GL.Rotatef(self.view_rotx, 1.0, 0.0, 0.0)
+        GL.Rotatef(self.view_roty, 0.0, 1.0, 0.0)
+        GL.Rotatef(self.view_rotz, 0.0, 0.0, 1.0)
+
+        GL.PushMatrix()
+        GL.Translatef(-3.0, -2.0, 0.0)
+        GL.Rotatef(self.angle, 0.0, 0.0, 1.0)
+        GL.CallList(self.gear1)
+        GL.PopMatrix()
+
+        GL.PushMatrix()
+        GL.Translatef(3.1, -2.0, 0.0)
+        GL.Rotatef(-2.0 * self.angle - 9.0, 0.0, 0.0, 1.0)
+        GL.CallList( 2)
+        GL.PopMatrix()
+
+        GL.PushMatrix()
+        GL.Translatef(-3.1, 4.2, 0.0)
+        GL.Rotatef(-2.0 * self.angle - 25.0, 0.0, 0.0, 1.0)
+        GL.CallList(self.gear3)
+        GL.PopMatrix()
+
+        GL.PopMatrix()
+
+        self.window.GL_SwapWindow()
+
+        self.frames += 1
+
+        t = SDL2.GetTicks()
+        if (t - self.time0) >= 5000:
+            seconds = (t - self.time0) / 1000.0
+            fps = self.frames / seconds
+            print("%d frames in %g seconds = %g FPS" % (self.frames,seconds,fps))
+            self.time0 = t
+            self.frames = 0
+
 
 def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
     r0 = inner_radius
@@ -144,171 +303,8 @@ def gear(inner_radius, outer_radius, width, teeth, tooth_depth):
         GL.Vertex3f(r0 * math.cos(angle), r0 * math.sin(angle), width * 0.5)
     GL.End()
 
-
-# new window size or exposure
-def reshape(width, height):
-    h = float(height) / float(width)
-    GL.Viewport(0, 0, width, height)
-    GL.MatrixMode(GL.PROJECTION)
-    GL.LoadIdentity()
-    GL.Frustum(-1.0, 1.0, -h, h, 5.0, 60.0)
-    GL.MatrixMode(GL.MODELVIEW)
-    GL.LoadIdentity()
-    GL.Translatef(0.0, 0.0, -40.0)
-
-
-def main():
-    Frames = 0
-    T0 = 0
-    gear1 = None
-    gear2 = None
-    gear3 = None
-    view_rotx = 20.0
-    view_roty = 30.0
-    view_rotz = 0.0
-    angle = 0.0
-
-    SDL.Init(SDL.INIT_VIDEO)
-
-    window = SDL.Window("SDLgears", (300,300), flags=SDL.WINDOW_OPENGL | SDL.WINDOW_RESIZABLE)
-    if not window:
-        print("Couldn't set 300x300 GL video mode: %s\n", SDL.GetError())
-        SDL.Quit()
-        sys.exit(2)
-
-    window.GL_CreateContext()
-
-    w,h = window.GetWindowSize()
-
-    pos = [5.0, 5.0, 10.0, 0.0]
-    red = [0.8, 0.1, 0.0, 1.0]
-    green = [0.0, 0.8, 0.2, 1.0]
-    blue = [0.2, 0.2, 1.0, 1.0]
-
-    GL.Lightfv(GL.LIGHT0, GL.POSITION, pos)
-    GL.Enable(GL.CULL_FACE)
-    GL.Enable(GL.LIGHTING)
-    GL.Enable(GL.LIGHT0)
-    GL.Enable(GL.DEPTH_TEST)
-
-    # make the gears
-    gear1 = GL.GenLists(1)
-    GL.NewList(gear1, GL.COMPILE)
-    GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, red)
-    gear(1.0, 4.0, 1.0, 20, 0.7)
-    GL.EndList()
-
-    gear2 = GL.GenLists(1)
-    GL.NewList(gear2, GL.COMPILE)
-    GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, green)
-    gear(0.5, 2.0, 2.0, 10, 0.7)
-    GL.EndList()
-
-    gear3 = GL.GenLists(1)
-    GL.NewList(gear3, GL.COMPILE)
-    GL.Materialfv(GL.FRONT, GL.AMBIENT_AND_DIFFUSE, blue)
-    gear(1.3, 2.0, 0.5, 10, 0.7)
-    GL.EndList()
-
-    GL.Enable(GL.NORMALIZE)
-
-    if len(sys.argv) > 1 and sys.argv[1] == "-info":
-        print("GL_RENDERER   =", GL.GetString(GL.RENDERER))
-        print("GL_VERSION    =", GL.GetString(GL.VERSION))
-        print("GL_VENDOR     =", GL.GetString(GL.VENDOR))
-        print("GL_EXTENSIONS =", GL.GetString(GL.EXTENSIONS))
-
-    reshape(w,h)
-
-    done = False
-    while not done:
-        angle += 2.0
-
-        while True:
-            event = SDL.PollEvent()
-            if not event:
-                break
-
-            if event[0] == SDL.QUIT:
-                done = True
-                break
-
-            #if event[0] == SDL.VIDEORESIZE:
-            #    window = SDL.SetVideoMode(event[1], event[2], 16, SDL.OPENGL | SDL.RESIZABLE)
-            #    if not window:
-            #        print('Bad resize event!')
-            #        sys.exit(-1)
-            #    reshape(window.w, window.h)
-
-        keys = SDL.GetKeyboardState()
-
-        if keys[SDL.K_ESCAPE]:
-            done = True
-
-        if keys[SDL.K_SPACE]:
-            spin = not spin
-
-#        if keys[SDL.K_UP]:
-#            view_rotx += 5.0
-#
-#        if keys[SDL.K_DOWN]:
-#            view_rotx -= 5.0
-#
-#        if keys[SDL.K_LEFT]:
-#            view_roty += 5.0
-#
-#        if keys[SDL.K_RIGHT]:
-#            view_roty -= 5.0
-#
-#        if keys[SDL.K_z]:
-#            if SDL.GetModState() & SDL.KMOD_SHIFT:
-#                view_rotz -= 5.0
-#            else:
-#                view_rotz += 5.0
-#
-        GL.Clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
-
-        GL.PushMatrix()
-        GL.Rotatef(view_rotx, 1.0, 0.0, 0.0)
-        GL.Rotatef(view_roty, 0.0, 1.0, 0.0)
-        GL.Rotatef(view_rotz, 0.0, 0.0, 1.0)
-
-        GL.PushMatrix()
-        GL.Translatef(-3.0, -2.0, 0.0)
-        GL.Rotatef(angle, 0.0, 0.0, 1.0)
-        GL.CallList(gear1)
-        GL.PopMatrix()
-
-        GL.PushMatrix()
-        GL.Translatef(3.1, -2.0, 0.0)
-        GL.Rotatef(-2.0 * angle - 9.0, 0.0, 0.0, 1.0)
-        GL.CallList(gear2)
-        GL.PopMatrix()
-
-        GL.PushMatrix()
-        GL.Translatef(-3.1, 4.2, 0.0)
-        GL.Rotatef(-2.0 * angle - 25.0, 0.0, 0.0, 1.0)
-        GL.CallList(gear3)
-        GL.PopMatrix()
-
-        GL.PopMatrix()
-
-        window.GL_SwapWindow()
-
-        Frames += 1
-
-        t = SDL.GetTicks()
-        if (t - T0) >= 5000:
-            seconds = (t - T0) / 1000.0
-            fps = Frames / seconds
-            print("%d frames in %g seconds = %g FPS" % (Frames,seconds,fps))
-            T0 = t
-            Frames = 0
-
-    del window
-
-    SDL.Quit()
-
 if '__main__' == __name__:
-    main()
-
+    try:
+        Gears()
+    except KeyboardInterrupt:
+        pass
